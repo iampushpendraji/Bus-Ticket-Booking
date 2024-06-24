1e0c013abb51b2520c350788c618f3a41fb1cceb
Pushpendra Sapkale <dev.psapkale@gmail.com>
2024-06-25 01:22:30 +0530

    Initialized Project

---
+++ bus-ticket-server/src/index.ts
@@ -0,0 +1,19 @@
+import express, { Request, Response } from 'express';
+
+// Create an Express application
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
