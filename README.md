78f7a2619afd7c792ed29e758e94a0c008ea394f
Pushpendra Sapkale <dev.psapkale@gmail.com>
2024-07-20 01:59:54 +0530

    Added mysqldemo.sql

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
 # Bus-Ticket-Booking
+
+## Description
+
+This project is created for learning purposes. It is a bus ticket booking application where users can book tickets for bus journeys. The project uses the latest version of Angular for the front-end and Node.js for the back-end. Additionally, it utilizes WebSockets for real-time updates and MySQL for the database.
+
+## Prerequisites
+
+- Node.js (latest version)
+- MySQL installed on your machine
+
+## Project Setup
+
+### UI Setup
+
+1. Navigate to the project directory:
+    ```bash
+    cd /Bus-Ticket-Booking/BUS-TICKET-UI
+    ```
+
+2. Install the dependencies:
+    ```bash
+    npm i
+    ```
+
+3. Start the UI server:
+    ```bash
+    npm start
+    ```
+
+### Server Setup
+
+1. Navigate to the project directory:
+    ```bash
+    cd /Bus-Ticket-Booking/BUS-TICKET-SERVER
+    ```
+
+2. Install the dependencies:
+    ```bash
+    npm i
+    ```
+
+3. Create a `.env` file and take reference from `/Bus-Ticket-Booking/BUS-TICKET-SERVER/env.js`.
+
+4. Import the `mysqldemo.sql` file into your MySQL database file path `/Bus-Ticket-Booking/BUS-TICKET-SERVER/mysqldemo.sql`.
+
+5. Start the server:
+    ```bash
+    npm start
+    ```
+
+## Additional Information
+
+- Ensure MySQL is running and properly configured.
+- Make sure to set up the database schema as required by the application.
+- For any issues, please check the logs or reach out to the support team.
---
+++ bus-ticket-server/mysqldemo.sql
@@ -0,0 +1,90 @@
+-- MySQL dump 10.13  Distrib 8.0.36, for macos14 (arm64)
+--
+-- Host: localhost    Database: bus-ticket-db
+-- ------------------------------------------------------
+-- Server version	8.3.0
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `auth_access_token`
+--
+
+DROP TABLE IF EXISTS `auth_access_token`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `auth_access_token` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `access_token` varchar(255) NOT NULL,
+  `refresh_token_id` int NOT NULL,
+  `created_on` varchar(255) NOT NULL,
+  `updated_on` varchar(255) NOT NULL,
+  `user_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `refresh_token_id` (`refresh_token_id`),
+  CONSTRAINT `auth_access_token_ibfk_1` FOREIGN KEY (`refresh_token_id`) REFERENCES `auth_refresh_token` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=41 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `auth_refresh_token`
+--
+
+DROP TABLE IF EXISTS `auth_refresh_token`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `auth_refresh_token` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `refresh_token` varchar(255) NOT NULL,
+  `user_id` int NOT NULL,
+  `created_on` varchar(255) NOT NULL,
+  `updated_on` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `idx_refresh_token` (`refresh_token`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `auth_refresh_token_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=59 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_first_name` varchar(45) NOT NULL,
+  `user_last_name` varchar(45) NOT NULL,
+  `user_email` varchar(45) NOT NULL,
+  `user_phone` varchar(15) DEFAULT NULL,
+  `user_type` varchar(45) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `created_on` varchar(255) NOT NULL,
+  `updated_on` varchar(255) NOT NULL,
+  `is_active` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `user_phone_UNIQUE` (`user_phone`)
+) ENGINE=InnoDB AUTO_INCREMENT=44 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-07-20  1:29:16
